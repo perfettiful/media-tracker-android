ecd05c875660bf877bf08e9c72a8e9e753e1c3ee
Nathan Perfetti <nathanperfetti@gmail.com>
2026-06-18 20:08:03 -0500

    use data object for the auth ui states

--- app/src/main/java/edu/metrostate/ics342/mediatracker/ui/auth/AuthViewModel.kt
+++ app/src/main/java/edu/metrostate/ics342/mediatracker/ui/auth/AuthViewModel.kt
@@ -20,9 +20,9 @@ class AuthViewModel(
 ) : ViewModel() {
 
     sealed class AuthUiState {
-        object Idle    : AuthUiState()
-        object Loading : AuthUiState()
-        object Success : AuthUiState()
+        data object Idle    : AuthUiState()
+        data object Loading : AuthUiState()
+        data object Success : AuthUiState()
         data class Error(val msgResId: Int) : AuthUiState()
     }
 
